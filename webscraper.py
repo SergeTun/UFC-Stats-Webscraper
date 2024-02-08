bf1c480686ca456257d087c79506df46d5787f15
Serge Thurain Bolduc Tun <sergethurain2@gmail.com>
2024-02-08 03:05:25 -0500

    it actually runs now

--- webscraper.py
+++ webscraper.py
@@ -1,24 +1,20 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 
 base_url = 'http://www.ufcstats.com/statistics/fighters?char='
 letters = 'abcdefghijklmnopqrstuvwxyz'
-path = '/Users/Serge/Downloads/chromedriver-win64/chromedriver.exe'
-
-driver = webdriver.Chrome(path)
+driver = webdriver.Chrome()
 
 for char in letters:
     url = base_url + char + '&page=all'
     driver.get(url)
 
-    try: 
-        WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'table.b-statistics__table')))
+    try:
 
-        fighter_links = driver.find_elements_by_css_selector('a.b-link_style_black')
+        fighter_links = driver.find_elements(By.CLASS_NAME, 'b-link.b-link_style_black')
 
         for link in fighter_links:
             fighter_name = link.text
@@ -27,8 +23,6 @@ for char in letters:
         soup = BeautifulSoup(driver.page_source, 'html.parser')
 
         driver.back()
-
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'table.b-statistics__table')))
     
     except Exception as e:
         print(f"Failed to retrieve data for {char}: {e}")
